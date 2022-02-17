0caf90f9d6dc7bc238465e14ba5ecb0965d8800c
Bartosz <bartosz.cylwik94@gmail.com>
2022-02-17 18:24:23 +0100

    add styles for moviepreview

--- src/components/MoviePreview.js
+++ src/components/MoviePreview.js
@@ -2,42 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { getSource } from '../utility/methods'
 
-const MoviePreview = ({ data: { attributes: { name, author, ytlink, yearOfPremiere, imageThumbnail } } }) => {
-  const [canAutoplay, setCanAutoplay] = React.useState(0)
-  const ytlinkReworked = ytlink.replace('watch?v=', 'embed/')
+const MoviePreview = ({ data: { attributes: { name, yearOfPremiere, imageThumbnail } } }) => {
 
   let namePath = name
   namePath = [...name].map(item => item === ' ' ? item = '-' : item).filter(item => item !== "'").join('').toLowerCase()
-
-  const isDesktop = window.innerWidth > 1024 ? true : false
-  // const isDesktop = false;
-  // React.useEffect(() => {
-  //   console.log(canAutoplay)
-
-  //   console.log(image)
-
-  // }, [canAutoplay])
 
   return (
     <div className='movie-preview-single-item'
     >
       <div className="movie-preview-thumbnail" >
-        {isDesktop &&
-          // <div id={`yt-${namePath}`}></div>
-          <iframe
-            title="YouTube video player"
-            src={`${ytlinkReworked}?autoplay=${canAutoplay}&mute=1&cc_load_policy=0&rel=0&modestbranding=1&autohide=1&showinfo=0&controls=0`}
-
-            allow='autoplay'
-            // autoPlay={true}
-            onMouseOver={() => setCanAutoplay(1)} onMouseOut={() => setCanAutoplay(0)}
-          >
-          </iframe>
-        }
-        {!isDesktop && <img src={getSource(imageThumbnail)
-        } alt={`Zdjęcie przedstawiające plakat filmu ${name}`} className='movie-preview-thumbnail__img' />}
+        <Link to={`/movies/${namePath}`}>
+          <img src={getSource(imageThumbnail)} alt={`Zdjęcie przedstawiające plakat filmu ${name}`} className={`movie-preview-thumbnail__img`} />
+        </Link>
 
       </div>
+
+
       <div className='movie-preview-info'>
         <Link to={`/movies/${namePath}`}>
           <h1 className="movie-preview-info__h1">{name}</h1>
