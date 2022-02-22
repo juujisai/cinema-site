a67486304bdbf09752620dd1bbd2aa807a489e0c
Bartosz <bartosz.cylwik94@gmail.com>
2022-02-22 16:41:15 +0100

    add footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import logo from '../media/cinema_logo.svg'
+import { Link } from 'react-router-dom'
+
+const menuData = [
+  {
+    path: '/',
+    name: 'home',
+  },
+  {
+    path: '/movies',
+    name: 'filmy'
+  },
+  {
+    path: '/rep',
+    name: 'repertuar'
+  },
+  {
+    path: '/info',
+    name: 'informacje'
+  },
+
+]
+
 
 const Footer = () => {
-  return ( 
-    <div>
-      Footer
-    </div>
-   );
+
+
+
+  const menu = menuData.map((item, id) => (
+    <li className='footer-menu__li' key={id} >
+      <Link to={item.path} className='footer-menu__li__a'  >{item.name}</Link>
+    </li >
+  ))
+
+
+  return (
+    <footer>
+      <h1 className="footer__h1">Zapraszamy do kina!</h1>
+
+      <ul className="footer-menu">
+        {menu}
+      </ul>
+      <div className="logo">
+        <img className='logo__img' src={logo} alt="logo kina cinemon" />
+      </div>
+
+    </footer>
+  );
 }
- 
+
 export default Footer;
