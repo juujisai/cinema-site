bfea2495f2f033a9f125683a1b44013fe036e198
Bartosz <bartosz.cylwik94@gmail.com>
2022-02-26 18:13:11 +0100

    wip movies page

--- src/page/MoviesPage.js
+++ src/page/MoviesPage.js
@@ -2,20 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux'
 import Loader from '../components/Loader'
 import MoviePreview from '../components/MoviePreview'
+import { IoFilter } from 'react-icons/io5'
 
 const MoviesPage = ({ movies }) => {
   const [cat, setCat] = React.useState()
 
 
   if (movies.loading) return <Loader />
+
   const moviesToShow = movies.movies.map((item, id) =>
     <MoviePreview key={id} data={item} />
   )
   console.log(cat)
   return (
     <div className="movies-page">
+
+
       <h1 className="main-header__h1">Aktualnie grane filmy </h1>
 
+      <div className="filter">
+        <div className="filter-controls">
+          <span className='filter-controls__span filter-controls__span--icon'><IoFilter /></span> <span className="filetr-controls__span">Filtruj</span>
+        </div>
+
+      </div>
       <div className="movies-container">
         {moviesToShow}
       </div>
